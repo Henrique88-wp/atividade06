e2d41cae152ca84b2949676264158455c9589632
Henrique88-wp <phdsn411@gmail.com>
2024-09-06 11:28:50 -0300

    atividade 06 do Sávio

---
+++ atividade06.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+
+a = input("escreva seu primeiro nome")
+b = input("escreva seu sobrenome")
+print(f"seu nome é {a} {b}")
